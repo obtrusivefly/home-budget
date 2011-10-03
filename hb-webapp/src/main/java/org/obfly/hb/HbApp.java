8be4fd14f46a35ea5b2c52da447ecda9d316fcf3
obtrusivefly <obtrusivefly@gmail.com>
2011-10-03 19:57:11 +0200

    Starting spring/wicket/hibernate configuration

--- hb-webapp/src/main/java/org/obfly/hb/HbApp.java
+++ hb-webapp/src/main/java/org/obfly/hb/HbApp.java
@@ -2,9 +2,16 @@ package org.obfly.hb;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.obfly.hb.page.HomePage;
 
 public class HbApp extends WebApplication{
+	
+    public void init() {
+        super.init();
+        getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+    }
+
 
 	@Override
 	public Class<? extends Page> getHomePage() {
--- hb-webapp/src/main/java/org/obfly/hb/page/HomePage.java
+++ hb-webapp/src/main/java/org/obfly/hb/page/HomePage.java
@@ -1,12 +1,11 @@
 package org.obfly.hb.page;
 
-import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
 public class HomePage extends WebPage {
 
-    public HomePage(final PageParameters parameters) {
+    public HomePage() {
     	 
         add(new Label("message", "Hello World, Wicket"));
  
